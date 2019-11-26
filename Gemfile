d6703f325f725d0fda1c088d123570faa66efee4
EC2 Default User <ec2-user@ip-172-31-86-7.ec2.internal>
2019-11-26 03:19:14 +0000

    done week11 with micropost

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ end
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',         '3.1.12'
 gem 'faker',          '1.7.3'
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
 gem 'will_paginate',           '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -57,6 +59,7 @@ group :development do
 end
 group :production do
   gem 'pg', '0.20.0'
+  gem 'fog', '1.42'
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
     @users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end
   def show
-    redirect_to root_url and return unless FILL_IN
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -65,7 +66,6 @@ class UsersController < ApplicationController
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
       redirect_to(root_url) unless current_user?(@user)
     end
 
